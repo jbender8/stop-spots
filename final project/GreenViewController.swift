3cddb3fe049bc20d3f523f5bf2b1962c0aedc711
Jessica Bender <jessicabender18@gmail.com>
2020-03-14 12:57:43 -0500

    Adding finals for final proj

---
+++ final project/GreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GreenViewController.swift
+//  final project
+//
+//  Created by Jessica Bender on 3/14/20.
+//  Copyright © 2020 Jessica Bender. All rights reserved.
+//
+
+import UIKit
+
+class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ final project/ShopsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  final project
+//
+//  Created by Jessica Bender on 3/14/20.
+//  Copyright © 2020 Jessica Bender. All rights reserved.
+//
+
+import UIKit
+
+class ShopsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
